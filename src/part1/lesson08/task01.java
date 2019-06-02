50555c8303a6cc5da858dd46e1133f8a16a56860
Alexander Mamonov <alexander.mamonov@protonmail.ch>
2019-06-02 18:19:39 +0300

    Lesson 05, task 01 second commit

---
+++ src/part1/lesson08/task01.java
@@ -0,0 +1,19 @@
+package part1.lesson08;
+
+/**
+ * Задание 1. Необходимо разработать класс, реализующий следующие методы:
+ *
+ * void serialize (Object object, String file);
+ * Object deSerialize(String file);
+ *
+ * Методы выполняют сериализацию объекта Object в файл file и десериализацию объекта из этого файла.
+ * Обязательна сериализация и десериализация "плоских" объектов (все поля объекта - примитивы, или String).
+ *
+ * @author Alexander.Mamonov@protonmail.ch
+ * @version 1.0
+ */
+public class task01 {
+    public static void main(String[] args) {
+
+    }
+}
