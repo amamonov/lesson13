cdb4c026a2a7f69eaa06e39bcd9388f9dd86c29a
Alexander Mamonov <alexander.mamonov@protonmail.ch>
2019-06-09 17:36:18 +0300

    Lesson 20, task 01 second commit

--- src/part1/lesson20/dock.sh
+++ src/part1/lesson20/dock.sh
@@ -1,6 +1,6 @@
 echo "Staring script" &&
 git pull https://www.github.com/amamonov/lesson20/ &&
-cd ./git/src/part1/lesson20/ &&
+cd ./src/part1/lesson20/ &&
 docker build -t java-app:demo . &&
 docker run java-app:demo &&
 echo "Script finish"
